532aa365810fe65a42abf23cfe15ae53fa8d4f91
Diyar Faraj <diyar.faraj@gmail.com>
2021-11-25 20:22:00 +0100

    added add post method, and sql connection

--- TravelApp/NewTravelPage.xaml.cs
+++ TravelApp/NewTravelPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
+using TravelApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,28 @@ namespace TravelApp
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_OnClicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text
+            };
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+            conn.Close();
+            if (rows > 0)
+                DisplayAlert("Success", "experince added succesfully", "OK");
+            else
+                DisplayAlert("FAil", "experince failed to add", "OK");
+
+            
+                
+            
+
+
         }
     }
 }
