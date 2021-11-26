d99853000c9b990c20af5dc0709380b455eb6255
Diyar Faraj <diyar@IS.local>
2021-11-26 21:36:27 +0100

    listview init

--- TravelApp/HistoryPage.xaml.cs
+++ TravelApp/HistoryPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
+using TravelApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,24 @@ namespace TravelApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HistoryPage : ContentPage
     {
+        public List<Post> myPosts = new List<Post>();
         public HistoryPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                var posts = conn.Table<Post>().ToList();
+                postListView.ItemsSource = posts;
+            }
+         
+
         }
     }
 }
--- TravelApp/NewTravelPage.xaml.cs
+++ TravelApp/NewTravelPage.xaml.cs
@@ -24,14 +24,18 @@ namespace TravelApp
             {
                 Experience = experienceEntry.Text
             };
-            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
-            conn.CreateTable<Post>();
-            int rows = conn.Insert(post);
-            conn.Close();
-            if (rows > 0)
-                DisplayAlert("Success", "experince added succesfully", "OK");
-            else
-                DisplayAlert("FAil", "experince failed to add", "OK");
+            
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(post);
+                if (rows > 0)
+                    DisplayAlert("Success", "experince added succesfully", "OK");
+                else
+                    DisplayAlert("FAil", "experince failed to add", "OK");
+            }
+            
+            
 
             
                 
